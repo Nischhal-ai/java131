edf6c22ddca843348e51ea0e0b8f59a9f33946bd
Nischhal Garg <nischhalgarg61@gmail.com>
2025-09-16 12:17:02 +0530

    16 SEP 2025 same prog13

---
+++ src/Unit3/prog14.java
@@ -0,0 +1,32 @@
+package Unit3;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class prog14 {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm= new HashMap<>();
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++){
+            String[] word= sc.nextLine().split(" ");
+            String query= word[0];
+            String key=word[1];
+            if (query.equals ("add")){
+                hm.put(key,hm.getOrDefault(key,0)+1);
+            }
+            else if(query.equals("count")){
+                System.out.println(hm.getOrDefault(key,0));
+            }
+            else if(query.equals("remove")){
+                if (hm.containsKey(key)){
+                    if(hm.getOrDefault(key,0)>1)
+                    hm.put(key,hm.get(key)-1);
+                    else
+                        hm.remove(key);
+                }
+            }
+        }
+    }
+}
