faa9afbbf3f96b14678260867bad543e00fe2d77
Nischhal Garg <nischhalgarg61@gmail.com>
2025-09-16 12:02:30 +0530

    16 SEP 2025 TREESET

---
+++ src/Unit3/prog11.java
@@ -0,0 +1,16 @@
+package Unit3;
+
+import java.util.TreeSet;
+
+public class prog11 {
+    public static void main(String[] args) {
+        TreeSet<String> tr=new TreeSet<>();
+        tr.add("one");
+        tr.add("five");
+        tr.add("four");
+        tr.add("tree");
+        tr.add("zebra");
+        tr.add("two");
+        System.out.println(tr);
+    }
+}
