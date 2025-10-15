cbc5f6edd553b60ac8b6078271e3bc3d5e50f79d
Nischhal Garg <nischhalgarg61@gmail.com>
2025-10-15 13:39:56 +0530

    15 OCT 2025 STREAM

---
+++ src/Unit2/main6.java
@@ -0,0 +1,35 @@
+package Unit2;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+/*
+public class main6 {
+    public static void main(String[] args) {
+        List<prog6> productlist=new ArrayList<>(); {
+            productlist.add(new prog6(2,"hjrgug",56);
+            productlist.add(new prog6(89,"hjrgug",6);
+            productlist.add(new prog6(21,"hjrgkfvug",666);
+            productlist.add(new prog6(25,"hjrgug",989);
+            productlist.add(new prog6(26,"hjuio",656);
+            productlist.add(new prog6(89,"hjrgujcfvjdjhdcg",55);
+            productlist.add(new prog6(55,"l",512);
+            productlist.add(new prog6(26,"a",589);
+            productlist.add(new prog6(90,"h",545);
+            productlist.add(new prog6(56,"h",567);
+            productlist.add(new prog6(78,"c",566);
+            productlist.add(new prog6(89,"s",532);
+            productlist.add(new prog6(55,"i",345);
+            productlist.add(new prog6(20,"n",789);
+            productlist.add(new prog6(26,"ity",989);
+            productlist.add(new prog6(298,"hiytyuio",906);
+            productlist.add(new prog6(245,"asme",879);
+            productlist.add(new prog6(265,"my",678);
+            productlist.add(new prog6(289,"hellooo",122);
+            productlist.add(new prog6(225,"hi",123);
+            List<Double> newList= productlist.stream().filter(p->p.price>120000).map(p->p.price).collect(Collectors.toList());
+            System.out.println(newList);
+        }
+    }
+}
+*/
---
+++ src/Unit2/prog6.java
@@ -0,0 +1,14 @@
+package Unit2;
+
+public class prog6 {
+    int id;
+    String name;
+    double price;
+
+    public prog6(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+
+        this.price = price;
+    }
+}
